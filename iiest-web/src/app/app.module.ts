110121c73717903ba7e34634e32f16a72ae38000
AjuDhiman <sairam87.ajay@gmail.com>
2023-09-15 16:18:04 +0530

    ..............................................................................

---
+++ iiest-web/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
+import{ FormsModule, ReactiveFormsModule} from '@angular/forms'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { SettingPanelComponent } from './shared/setting-panel/setting-panel.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    AboutComponent,
+    FooterComponent,
+    SidebarComponent,
+    SettingPanelComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
