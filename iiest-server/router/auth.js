c4fbc1cc4b1bab754704f67aa43f04d1a5f99f5e
Bhavesh Daipuria <bdaipuria@gmail.com>
2023-09-20 05:03:56 +0530

    Implemented password hashing and jwt generation on login

--- iiest-server/router/auth.js
+++ iiest-server/router/auth.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const db = require('../config/db');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const JWT_SECRET = process.env.JWT_TOKEN;
 
 //Route for staff entry
 router.post('/staffentry', 
 
-//Validation for backend using express-validator 
+//Validation for registration using express-validator 
 [
     body('employee_name', 'Enter a valid name').isLength({min: 3}).exists(),
     body('gender', 'Enter a valid gender').isLength({min: 4}).exists(),
@@ -30,7 +34,8 @@ router.post('/staffentry',
 ], 
 async(req, res)=>{
 
-    //Fields to be used for submission
+    //Fields being used for staff entry
+    let success = false;
     const { employee_name, gender, email, contact_no, alternate_contact, dob, address, zip_code, employee_id, portal_type, department, designation, salary, grade_pay, doj, company_name, project_name, username, password } = req.body;
     const errors = validationResult(req);
 
@@ -38,39 +43,71 @@ async(req, res)=>{
         return res.status(500).json({errors: errors.array()});
     }
 
-    //Running sql query for registration
-    const sql = `INSERT INTO new_registers (employee_name, gender, email, contact_no, alternate_contact, dob, address, zip_code, employee_id, portal_type, department, designation, salary, grade_pay, doj, company_name, project_name, username, password) VALUES ('${employee_name}', '${gender}', '${email}', '${contact_no}', '${alternate_contact}', '${dob}', '${address}', '${zip_code}', '${employee_id}', '${portal_type}', '${department}', '${designation}', '${salary}', '${grade_pay}', '${doj}', '${company_name}', '${project_name}', '${username}', '${password}')`; 
+    //Password Hashing
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(password, salt);
+
+    //SQL query for staff entry
+    const sql = `INSERT INTO new_registers (employee_name, gender, email, contact_no, alternate_contact, dob, address, zip_code, employee_id, portal_type, department, designation, salary, grade_pay, doj, company_name, project_name, username, password) VALUES ('${employee_name}', '${gender}', '${email}', '${contact_no}', '${alternate_contact}', '${dob}', '${address}', '${zip_code}', '${employee_id}', '${portal_type}', '${department}', '${designation}', '${salary}', '${grade_pay}', '${doj}', '${company_name}', '${project_name}', '${username}', '${secPass}')`; 
 
     db.query(sql, [employee_name, gender, email, contact_no, alternate_contact, dob, address, zip_code, employee_id, portal_type, department, designation, salary, grade_pay, doj, company_name, project_name, username, password], (err, result)=>{
         if(err){
-            console.log(err)
-            res.status(500).json({error: 'Registration failed'});
+            success = false;
+            console.log(err);
+            res.status(500).json({success, error: 'Registration failed'});
         }else{
-            res.status(200).json({message: 'Resgistration Successful'});
+            success = true;
+            res.status(200).json({success, message: 'Resgistration Successful'});
         }
     });
 })
 
-//Only for testing purpose: To grab info from database by entering the username and password
-router.post('/login', [
-    body('username', 'Enter a valid email').exists(),
-    body('password', 'Enter a valid password').exists()
-], async(req, res)=>{
+
+//Route for staff login
+router.post('/login', 
+
+//Validation for login using express-validator 
+[
+body('username', 'Enter a valid username').exists(),
+body('password', 'Enter a valid password').exists()
+],
+async(req, res) => {
+
+    let success = false;
+
     const { username, password } = req.body;
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()});
-    }
+  
+    // SQL query to check member with the entered username in the database
+    const sql = `SELECT * FROM new_registers WHERE username = '${username}'`
+    db.query(sql, username, (err, rows) => {
 
-    //Running sql query for demo login
-    const sql = `SELECT * FROM new_registers where username='${username}' AND password='${password}'`;
-    db.query(sql, [username, password], (err, result)=>{
-        if(err){
-            res.status(500).json({error: 'Login Failed'})
-        }else{
-            res.status(200).json({message: result})
+        if (err) {
+        return res.status(500).json({success, message: 'Some error occured' });
+        } else if (rows.length === 0) {
+        return res.status(401).json({success, message: 'Please try to login with correct credentials' });
         }
+  
+        const user = rows[0];
+
+        //Comparing Hashed Password
+        bcrypt.compare(password, user.password,  (err, result)=>{
+            if(err || !result){
+                success = false
+                return res.status(401).json({success, message: 'Please try to login with correct credentials' });
+            }
+
+            //Generating a JWT Token after successful login
+            const data = {
+                user:{
+                    id: user.s_no
+                }
+            }
+            const authToken = jwt.sign(data,  JWT_SECRET);
+            success = true;
+            res.status(200).json({ success, authToken });
+        }) 
+     
     });
-})
+  });
 
 module.exports = router;
--- iiest-web/src/app/signup/signup.component.ts
+++ iiest-web/src/app/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators,FormControl } from '@angular/forms';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -20,21 +20,22 @@ export class SignupComponent {
     department: string = '';
     dob: string = '';
     designation: string = '';
-    salary: number = 0;
-    grade_pay: number = 500;
+    salary: number = NaN;
+    grade_pay: number = NaN;
     company_name: string = '';
     project_name: string = '';
     doj: string = '';
     email: string = '';
-    contact_no: number = 0;
-    alternate_contact: number = 0;
+    contact_no: number = NaN;
+    alternate_contact: number = NaN;
     address: string = '';
-    zip_code: number = 0;
+    zip_code: number = NaN;
 
-    // adding form validators
     constructor(private http: HttpClient){}
 
-    signupForm=new FormGroup({
+
+    //Form Validation
+    signupForm = new FormGroup({
         employee_name: new FormControl('', [Validators.required]),
         gender: new FormControl('', [Validators.required]),
         username: new FormControl('',[Validators.required]),
@@ -54,38 +55,7 @@ export class SignupComponent {
         alternate_contact: new FormControl('', [Validators.pattern(/^[0-9]{10}$/)]),
         address: new FormControl('', [Validators.required]),
         zip_code: new FormControl(0, [Validators.required,Validators.pattern(/^[0-9]{6}$/)])
-      
-
     });
-
-
-
-    // signupForm: FormGroup;
-
-
-    // constructor(private http: HttpClient, private fb:FormBuilder){
-    //   this.signupForm = this.fb.group({
-    //     employee_name: ['', Validators.required],
-    //     gender: ['', Validators.required],
-    //     username: ['', Validators.required],
-    //     password: ['', [Validators.required, Validators.minLength(6)]],
-    //     employee_id: ['', Validators.required],
-    //     portal_type: ['', Validators.required],
-    //     department: ['', Validators.required],
-    //     dob: ['', Validators.required],
-    //     designation: ['', Validators.required],
-    //     salary: [0, Validators.required], // Set a default value
-    //     grade_pay: [500, Validators.required], // Set a default value
-    //     company_name: ['', Validators.required],
-    //     project_name: ['', Validators.required],
-    //     doj: ['', Validators.required],
-    //     email: ['', [Validators.required, Validators.email]],
-    //     contact_no: [0, [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/)]],
-    //     alternate_contact: [0, Validators.pattern(/^-?(0|[1-9]\d*)?$/)],
-    //     address: ['', Validators.required],
-    //     zip_code: [0, [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/)]]
-    //   });
-    // }
 
     submitForm(){
       const postData = {
@@ -110,14 +80,12 @@ export class SignupComponent {
         zip_code: this.zip_code
       }
 
-      console.log(postData)
-
       this.http.post('http://localhost:3000/auth/staffentry', postData).subscribe(
         (response)=>{
-          console.log('POST request successful: ', response)
+          console.log('Registation request successful: ', response)
         },
         (error)=>{
-          console.log('POST Request Error', error)
+          console.log('Registration Request Error', error)
         }
       )
     }
