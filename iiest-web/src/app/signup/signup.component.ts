151a07658253a204380a2c236df2170b76e7a83f
Arjav <1971arjavbarya@gmail.com>
2023-09-20 01:13:19 +0530

    Empty form submission errors

--- iiest-web/src/app/signup/signup.component.ts
+++ iiest-web/src/app/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators,FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators,FormControl, } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -11,6 +11,8 @@ import { FormBuilder, FormGroup, Validators,FormControl } from '@angular/forms';
 
 
 export class SignupComponent {
+  submitted=false;
+
     employee_name: string = '';
     gender: string = '';
     username: string = '';
@@ -60,33 +62,6 @@ export class SignupComponent {
 
 
 
-    // signupForm: FormGroup;
-
-
-    // constructor(private http: HttpClient, private fb:FormBuilder){
-    //   this.signupForm = this.fb.group({
-    //     employee_name: ['', Validators.required],
-    //     gender: ['', Validators.required],
-    //     username: ['', Validators.required],
-    //     password: ['', [Validators.required, Validators.minLength(6)]],
-    //     employee_id: ['', Validators.required],
-    //     portal_type: ['', Validators.required],
-    //     department: ['', Validators.required],
-    //     dob: ['', Validators.required],
-    //     designation: ['', Validators.required],
-    //     salary: [0, Validators.required], // Set a default value
-    //     grade_pay: [500, Validators.required], // Set a default value
-    //     company_name: ['', Validators.required],
-    //     project_name: ['', Validators.required],
-    //     doj: ['', Validators.required],
-    //     email: ['', [Validators.required, Validators.email]],
-    //     contact_no: [0, [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/)]],
-    //     alternate_contact: [0, Validators.pattern(/^-?(0|[1-9]\d*)?$/)],
-    //     address: ['', Validators.required],
-    //     zip_code: [0, [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/)]]
-    //   });
-    // }
-
     submitForm(){
       const postData = {
         employee_name: this.employee_name,
@@ -110,6 +85,16 @@ export class SignupComponent {
         zip_code: this.zip_code
       }
 
+
+      this.submitted=true
+      if(this.signupForm.invalid){
+        return
+      }
+
+
+      
+    
+
       console.log(postData)
 
       this.http.post('http://localhost:3000/auth/staffentry', postData).subscribe(
@@ -120,5 +105,10 @@ export class SignupComponent {
           console.log('POST Request Error', error)
         }
       )
+    }
+
+    Reset(){
+      this.submitted=false;
+      this.signupForm.reset();
     }
 }
